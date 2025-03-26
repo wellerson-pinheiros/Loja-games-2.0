1a8b1ed6fbe2b26af9abad943c476ebf90fbb82c
wellerson pinheiros <wellersonpinheiros@outlook.com>
2025-03-26 19:18:16 -0300

    :sparkles: feat/ Criando a entidade Jogos para o banco de dados

---
+++ src/jogos/entities/Jogos.entity.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty, isNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name:'tb_jogos'})
+
+export class Jogos {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 100} )
+    nomeJogo: string;
+
+    @IsNotEmpty()
+    @Column({type: "decimal", precision: 10, scale: 2 ,nullable: false})
+    precoJogo: number;
+
+    @Column({ type: "text", nullable: true })
+    descricao: string;
+
+    @Column ({type: "text", nullable:true })
+    fotoJogo: string;
+     
+    @Column({type: "date", nullable: true})
+    dataLançamento: Date
+}
